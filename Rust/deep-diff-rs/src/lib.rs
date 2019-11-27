d082d329777d3381b25336a4556366e64943f867
hokuron <anti.soft.b@gmail.com>
2019-11-27 19:18:00 +0900

    Clone Heckel algorithm in Swift into Rust

--- Rust/deep-diff-rs/src/lib.rs
+++ Rust/deep-diff-rs/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::os::raw::{c_char, c_int};
+use std::ffi::CStr;
+use std::slice;
+
+mod hackel;
+
+#[no_mangle]
+pub extern fn diffWithString(old: *const *const c_char, old_len: c_int, new: *const *const c_char, new_len: c_int) {
+    let old = unsafe { collect_strs_from(old, old_len) };
+    let new = unsafe { collect_strs_from(new, new_len) };
+    hackel::diff(&old, &new);
+}
+
+unsafe fn collect_strs_from(array_of_c_char: *const *const c_char, len: c_int) -> Vec<&'static str> {
+    slice::from_raw_parts(array_of_c_char, len as usize)
+        .iter()
+        .map( |char| CStr::from_ptr(*char).to_str().unwrap())
+        .collect()
 }
